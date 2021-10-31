72de7085659ecbc08d97c7e52452467f9e472b5b
Vladimir Zagray <snipervld@gmail.com>
2021-11-01 02:09:57 +0300

    fix publish script

--- publish.ps1
+++ publish.ps1
@@ -9,4 +9,4 @@ if (-not (Test-Path artefacts/*.nupkg))
     throw 'no packages to publish'
 }
 
-dotnet nuget push artefacts/*.nupkg -k $AppKey -s https://api.nuget.org/v3/index.json
+dotnet nuget push "artefacts\*.nupkg" -k $AppKey -s https://api.nuget.org/v3/index.json
